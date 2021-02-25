3578dffe30e8e8a270d01fbdb2b0c6eef76121d5
sunshuchao <www.sunshuchao@outlook.com>
2021-02-25 16:49:46 +0800

    学习打AB包

---
+++ Assets/Editor/TestAssetBundle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAssetBundle
+{
+    [MenuItem("AB/buildAB")]
+    public static void TestBuildAb()
+    {
+        string buildPath = "Assets/AssetBundles";
+        if (!Directory.Exists(buildPath))
+        {
+            Directory.CreateDirectory(buildPath);
+        }
+
+        BuildPipeline.BuildAssetBundles(buildPath, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows);
+    }
+}
