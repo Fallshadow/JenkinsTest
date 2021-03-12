10f70db025cb0a79112e136ae02b8ca714387e13
packPC <www.sunshuchao@outlook.com>
2021-03-12 13:39:16 +0800

    update:更新

---
+++ 自动化构建流程PY代码/build_android.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import localutil
+
+if __name__ == '__main__':
+    localutil.log('1')
---
+++ 自动化构建流程PY代码/localutil.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import datetime
+import os
+import sys
+
+
+# 执行系统命令
+def execute(cmd):
+    log("execute cmd >> " + cmd)
+    os.system(cmd)
+
+
+# 输出带时间的log
+def log(*args):
+    logMsg = "[log][" + get_current_time() + "]"
+    for item in args:
+        logMsg = logMsg + str(item)
+    print(logMsg)
+
+
+# 获得当前时间
+def get_current_time():
+    now_time = datetime.datetime.now()
+    time_str = datetime.datetime.strftime(now_time,'%Y-%m-%d %H:%M:%S')
+    return time_str
+
+
+# 检测该脚本是否被禁用
+def check_disable():
+    isDisable = False
+    if len(sys.argv) > 1:
+        for item in sys.argv:
+            if item == "Disable" or item == "disable":
+                isDisable = True
+    return isDisable
